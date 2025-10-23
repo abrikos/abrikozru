f91c0b781eec3b6f75339ac4d90a8118964b5f9b
Artem Filippow <a.filippov@qtech.ru>
2025-10-23 16:14:08 +0900

    docker

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Stage 1: Build the Nuxt application
+FROM node:22-alpine AS build
+WORKDIR /app
+
+# Enable Corepack for package manager management (e.g., pnpm)
+RUN corepack enable
+
+# Copy package.json and lockfile (e.g., pnpm-lock.yaml)
+COPY package.json yarn.lock ./
+
+# Install dependencies
+RUN yarn
+
+# Copy the entire project
+COPY . ./
+
+# Build the Nuxt application for production
+# Assuming 'npm run build' or 'pnpm build' is configured to build for production
+ARG MONGODB_URI
+RUN echo "zzzzzzzzzzzzz ${MONGODB_URI}"
+ENV MONGODB_URI=$MONGODB_URI
+RUN yarn build
+
+# Stage 2: Serve the Nuxt application
+FROM node:22-alpine AS runner
+
+WORKDIR /app
+
+# Copy only necessary files from the build stage
+COPY --from=build /app/.output ./.output
+COPY --from=build /app/package.json ./package.json
+# Command to start the Nuxt server
+CMD ["node", ".output/server/index.mjs"]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  modules: [
+    'nuxt-mongoose',
+    'nuxt-quasar-ui',
+  ],
+  mongoose: {
+    modelsDir: 'models',
+  },
+  quasar: {
+    lang: 'ru',
+    extras: {
+      fontIcons: ['material-icons']
+    },
+    //sassVariables: '~/public/quazar.variables.sass',
+    components: {
+      defaults: {
+        QBtnDropdown: {
+          dense: true,
+          flat: true,
+          noCaps: true
+        },
+        QBtn: {
+          dense: true,
+          flat: false,
+          noCaps: true
+        },
+        QInput: {
+          outlined: true,
+          dense: true,
+        },
+        QSelect: {
+          outlined: true,
+          dense: true,
+          optionsDense: true
+        },
+      }
+    }
+  },
 })
